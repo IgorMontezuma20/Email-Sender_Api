4557818d76d14f71cd55703db20cb07a75b44b41
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-08 21:33:36 -0300

    feat: insert rabbitmq and queues in application

--- src/main/java/com/ms/email/models/EmailModel.java
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -3,29 +3,9 @@ package com.ms.email.models;
 import com.ms.email.enums.EmailStatus;
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
-
-//@Data
-//@Entity
-//@Table(name = "TB_EMAIL")
-//public class EmailModel implements Serializable {
-//    private static final long serialVersionUID = 1L;
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long emailId;
-//    private String ownerRef;
-//    private String emailFrom;
-//    private String emailTo;
-//    private String subject;
-//    @Column(columnDefinition = "TEXT")
-//    private String text;
-//    private LocalDateTime sendEmailDate;
-//    private EmailStatus emailStatus;
-//}
-
+import java.util.UUID;
 
 @Data
 @Entity
@@ -35,7 +15,7 @@ public class EmailModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    private Long emailId;
+    private UUID emailId;
     private String ownerRef;
     private String emailFrom;
     private String emailTo;
